cb21b9b7e9740267c6fd2fa5df09174a0907cf37
Aibek002 <200103346@stu.sdu.edu.kz>
2024-02-06 00:14:52 +0600

    Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -4,10 +4,16 @@ $connection = new PDO('mysql:host=localhost;dbname=fitnes;charset=utf8;', 'root'
 $name = 'Arailym';
 $surname = 'Seidakhmet';
 $kg = 55;
-$query=" INSERT  fitnes(name , surname , kg ) VALUE ( '$name' , '$surname' , 55 )";
 
-// $query = "INSERT INTO fitnes (name, surname, kg) VALUES ('$name', '$surname', $kg)";
-$result = $connection->exec($query);
+$parametr=[
+    'n' => $name,
+    's' => $surname,
+    'k' => $kg,
+];
+$query=" INSERT  fitnes(name , surname , kg ) VALUE ( :n , :s , :k )";
+$sql=$connection->prepare($query);
+$sql->execute($parametr);
+
 
 if ($result !== false) {
     echo "Data inserted successfully.";
