f1d65d25e831493d4b3c92ad2d4b6e1bd7f99ea6
Aibek002 <200103346@stu.sdu.edu.kz>
2024-02-07 12:04:45 +0600

    Changes to be committed: 	modified:   insert.php

--- insert.php
+++ insert.php
@@ -1,28 +1,74 @@
 
     <?php
     require 'settings.php';
-    function prints($value){
+
+
+// --prints data----
+    function prints($value)
+    {
         echo '<pre>';
         print_r($value);
         '</pre>';
     }
-    function dbCheckError($query){
-        $errInfo=$query->errorInfo();
-        if($errInfo[0] !== PDO::ERR_NONE){
+
+
+// ---check error on data--
+    function dbCheckError($query)
+    {
+        $errInfo = $query->errorInfo();
+        if ($errInfo[0] !== PDO::ERR_NONE) {
             echo $errInfo[2];
             exit();
         }
         return true;
     }
-
-    function selectAll($table){
+// --request on database--
+    function selectAll($table)
+    {
         global $connection;
-        $sql='SELECT * FROM ' . $table ;
-        $query=$connection->prepare($sql);
+        $sql = 'SELECT * FROM ' . $table;
+        $query = $connection->prepare($sql);
         $query->execute();
         dbCheckError($query);
 
         return $query->fetchAll();
     }
+
+
+    // ---insert data---
+    function insert($table , $parram){
+        global $connection;
+        $i=0;
+        $coll='';
+        $mask='';
+
+        foreach($parram as $key => $value){
+            if($i===0){
+                $coll=$coll . " $key ";
+                $mask=$mask . "'". " $value " . "'";
+                
+            }else{
+                $coll=$coll . "," . " $key ";
+                $mask=$mask . ","."'". " $value " . "'";
+            }
+            $i++;
+        }
+        $sql="INSERT INTO $table ($coll) VALUES ($mask)";
+        print($sql);
+        // exit();
+        $query = $connection->prepare($sql);
+        $query->execute();
+        dbCheckError($query);
+
+    }
+   
+    $arrData=[
+        'name'=>'Aibek',
+        'surname'=>'Aibek',
+        'kg'=>'88',
+    ];
+    insert('fitnes',$arrData);
     prints(selectAll('fitnes'))
-?>
+
+
+    ?>
