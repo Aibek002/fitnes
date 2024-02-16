3763214f9bd255b15ca161c74ba9f345a2e95d00
Aibek002 <200103346@stu.sdu.edu.kz>
2024-02-16 10:32:36 +0600

    modified:   include/header.php

--- include/header.php
+++ include/header.php
@@ -12,15 +12,21 @@
       <li class="nav-item"><a href="<? echo BASE_URL . 'fitneses.php' ?>" class="nav-link">Fitneses</a></li>
       <li class="nav-item"><a href="<? echo BASE_URL . 'about.php' ?>" class="nav-link">AboutUs</a></li>
       <li class="nav-item"><a href="<? echo BASE_URL . 'faqs.php' ?>" class="nav-link">FAQs</a></li>
-      <? if (isset($_SESSION['id'])) : ?>
-        <li class="nav-item"><a href="#" class="nav-link "><?php echo $_SESSION['name']  ?></a></li>
-        <li class="nav-item"><a href="<? echo BASE_URL . 'logout.php' ?>" class="nav-link active">logout</a></li>
+      <?php if (isset($_SESSION['id'])) : ?>
+        <li class="nav-item"><a href="#" class="nav-link"><?php echo $_SESSION['name']; ?></a></li>
+        <li class="nav-item"><a href="<?php echo BASE_URL . 'logout.php'; ?>" class="nav-link active">Logout</a></li>
+      <?php else : ?>
+        <?php if ($_SERVER['REQUEST_URI'] === '/login.php') : ?>
+          <li class="nav-item"><a href="<?php echo BASE_URL . 'register.php'; ?>" class="nav-link active">Sign in</a></li>
+          <hr />
+          <li class="nav-item"><a href="<?php echo BASE_URL . 'login.php'; ?>" class="nav-link"></a></li>
+        <?php else : ?>
+          <li class="nav-item"><a href="<?php echo BASE_URL . 'register.php'; ?>" class="nav-link active">Sign in</a></li>
+          <hr />
+          <li class="nav-item"><a href="<?php echo BASE_URL . 'login.php'; ?>" class="nav-link active">Sign up</a></li>
+        <?php endif; ?>
+      <?php endif; ?>
 
-      <? else : ?>
-        <li class="nav-item"><a href="<? echo BASE_URL . 'register.php' ?>" class="nav-link active">Sign in</a></li>
-        <hr />
-        <li class="nav-item"><a href="<? echo BASE_URL . 'login.php' ?>" class="nav-link active">Sign up</a></li>
-      <? endif ?>
     </ul>
   </header>
 </div>
