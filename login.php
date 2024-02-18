45f0104454d70ad9a5c28487f40e5019c01daed6
Aibek002 <200103346@stu.sdu.edu.kz>
2024-02-18 20:28:56 +0600

    modified:   login.php 	modified:   signinemail.php

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ include './connectDB/user.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./assets/css/register.css">
     <title>Document</title>
 </head>
 
@@ -49,7 +49,7 @@ include './connectDB/user.php';
                 <p>Or</p>
                 <button type="button" class="Signemail-btn">Sign in with email</button>
                 <a href="#" class="social-link"><img src="facebook-icon.png" alt="Facebook"></a>
-                <a href="#" class="social-link"><img src="google-icon.png" alt="Google"></a>
+                <a href="signinemail.php" class="social-link"><img src="google-icon.png" alt="Google"></a>
             </div>
         </div>
     </main>
--- signinemail.php
+++ signinemail.php
@@ -11,7 +11,7 @@ include './connectDB/user.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./assets/css/register.css">
     <title>Document</title>
 </head>
 
@@ -31,15 +31,15 @@ include './connectDB/user.php';
             <div class="text1">
                 <h3>Sign in with Email</h3>
             </div>
-            <form action="#" method="post">
+            <form action="signinemail.php" method="post">
                 
                 <div class="input-group">
-                    <input type="email" id="Email" placeholder="Email" required>
+                    <input name="email" type="email" id="Email" placeholder="Email" required>
                 </div>
                 <div class="input-group">
-                    <input type="password" id="Password" placeholder="Password" required>
+                    <input name='password' type="password" id="Password" placeholder="Password" required>
                 </div>
-                <button type="submit" class="continue-btn">Continue</button>
+                <button name='btn-log' type="submit" class="continue-btn">Continue</button>
             </form>
             
         </div>
