36ded302599e3559402fdb1a5e6b8bdc1c3b1cc7
Aibek002 <200103346@stu.sdu.edu.kz>
2024-02-18 20:03:21 +0600

    new file:   assets/css/register.css 	modified:   register.php

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ include './connectDB/user.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="styles.css">
+  <link href="assets/css/register.css" rel="stylesheet">
   <title>Document</title>
 </head>
 
@@ -36,7 +36,7 @@ include './connectDB/user.php';
     </div>
       <form action="register.php" method="post">
         <div class="input-group">
-          <input value='<?= $name ?>' name='name' type="text" id="Name" placeholder="Name" required>
+          <input value='<?= $name ?>' na me='name' type="text" id="Name" placeholder="Name" required>
         </div>
         <div class="input-group">
           <input value='<?= $surname ?>' name='surname' type="text" id="Username" placeholder="Surnname" required>
