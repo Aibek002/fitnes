0c2c7179566f362a701735e0ba4298ae0804e7e5
Aibek002 <200103346@stu.sdu.edu.kz>
2024-02-20 12:22:58 +0600

    modified:   assets/css/register.css 	modified:   register.php

--- register.php
+++ register.php
@@ -25,15 +25,27 @@ include './connectDB/user.php';
   <main>
     <div class="container">
       <div class="text">
-        <button type="button" class="back-btn"><img src="back-icon.png" width="100%" alt="Back"></button>
-        <h1>Sign Up</h1>
+        <button type="button" class="back-btn"><img src="back-icon.png" alt="Back"></button>
+        <h1 class="register-logo">
+          <svg xmlns="http://www.w3.org/2000/svg" width="23" height="20" viewBox="0 0 23 20" fill="none">
+            <mask id="mask0_111_120" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="23" height="20">
+              <rect width="25.0799" height="10" transform="matrix(0.91707 0.398726 0 1 0 0)" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_111_120)">
+              <rect width="25.0799" height="10" transform="matrix(0.91707 -0.398726 0 1 0 0)" fill="#1D3EB3" />
+              <rect width="25.0799" height="25.0799" transform="matrix(0.91707 0.398726 -0.91707 0.398726 11.8477 4.54492)" fill="#FAC02B" />
+            </g>
+          </svg>
+
+          <span style="display: inline-block; vertical-align: middle;">Register</span>
+        </h1>
       </div>
       <div class="text1">
-        <h3>Sign up with Email</h3>
+        <p>Already have an account? <a href="/signinemail.php">Log In</a></p>
       </div>
       <div class="form-floating">
-      <p for="floatingInput"><?=$errMsgEmpty?></p>
-    </div>
+        <p for="floatingInput"><?= $errMsgEmpty ?></p>
+      </div>
       <form action="register.php" method="post">
         <div class="input-group">
           <input value='<?= $name ?>' name='name' type="text" id="Name" placeholder="Name" required>
