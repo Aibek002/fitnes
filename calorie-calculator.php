a3c65e21327bccab3ec29da0382ee69c32421f17
Aibek002 <200103346@stu.sdu.edu.kz>
2024-03-08 01:34:41 +0600

    deleted:    assets/images/Iconly/Two-tone/Notification.png 	new file:   assets/images/calorie-calculator-image/Avocado.png 	new file:   assets/images/calorie-calculator-image/Beet.png 	new file:   assets/images/calorie-calculator-image/Broccoli.png 	new file:   assets/images/calorie-calculator-image/bread.png 	new file:   assets/images/calorie-calculator-image/donut.png 	new file:   assets/images/calorie-calculator-image/paleo.png 	new file:   calorie-calculator.php 	modified:   printCalorieCalculate.php

---
+++ calorie-calculator.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Calory Calculator</title>
+</head>
+<body>
+    <p>Ready to give it a shot ?        Let us know your diet</p>
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <img src="./assets/images/calorie-calculator-image/donut.png" alt="">
+      <p>Anything</p>
+    </div>
+    <div class="col">
+    <img src="./assets/images/calorie-calculator-image/paleo.png" alt="">
+      <p>Paleo</p>
+    </div>
+    <div class="col">
+    <img src="./assets/images/calorie-calculator-image/Broccoli.png" alt="">
+      <p>Vegetarian</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+    <img src="./assets/images/calorie-calculator-image/Avocado.png" alt="">
+      <p>Vegan</p>
+    </div>
+    <div class="col">
+    <img src="./assets/images/calorie-calculator-image/bread.png" alt="">
+      <p>Ketogenic</p>
+    </div>
+    <div class="col">
+    <img src="./assets/images/calorie-calculator-image/Beet.png" alt="">
+      <p>Mediterranean</p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
