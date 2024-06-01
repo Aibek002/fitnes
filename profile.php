faeea8be50363d14efae5953033ec2aae32ee017
Aibek002 <200103346@stu.sdu.edu.kz>
2024-06-01 09:02:26 +0500

    modified:   assets/css/profile.css 	new file:   assets/images/profileImage/cheese.png 	new file:   assets/images/profileImage/fish.png 	new file:   assets/images/profileImage/toasterBread.png 	new file:   assets/images/profileImage/wheat.png 	modified:   profile.php

--- profile.php
+++ profile.php
@@ -57,6 +57,7 @@ if (trim($_SESSION['email']) == 'admin@gmail.com') {
 
 
             <div class="flex-second-item">
+                <img class='spot' src="./assets/images/profileImage/bgSpot.png" alt="">
                 <div class="logo_user">
                     <img src="./assets/images/profileImage/logo_user.png" alt="logo_user">
                 </div>
@@ -107,25 +108,38 @@ if (trim($_SESSION['email']) == 'admin@gmail.com') {
 
             </div>
             <div class="flex-third-item">
+
                 <div class="protein">
-                    <p>Protein</p>
-                    <hr style="width: 65px; height: 2px; background-color: white; border: none;">
-                    <strong>94 g</strong>
+                    <div class="first-element"><img src="./assets/images/profileImage/fish.png" alt=""></div>
+                    <div class="second-element">
+                        <p>Protein</p>
+                        <hr class="hr-third-flex">
+                        <strong>94 g</strong>
+                    </div>
                 </div>
                 <div class="protein">
-                    <p>Metabolic</p>
-                    <hr style="width: 65px; height: 2px; background-color: white; border: none;">
-                    <strong>94 </strong>
+                    <div class="first-element"><img src="./assets/images/profileImage/wheat.png" alt=""></div>
+                    <div class="second-element">
+                        <p>Metabolic</p>
+                        <hr class="hr-third-flex">
+                        <strong>94 </strong>
+                    </div>
                 </div>
                 <div class="protein">
-                    <p>Carbs</p>
-                    <hr style="width: 65px; height: 2px; background-color: white; border: none;">
-                    <strong>94 g</strong>
+                    <div class="first-element"><img src="./assets/images/profileImage/toasterBread.png" alt=""></div>
+                    <div class="second-element">
+                        <p>Carbs</p>
+                        <hr class="hr-third-flex">
+                        <strong>94 g</strong>
+                    </div>
                 </div>
                 <div class="protein">
-                    <p>Fat</p>
-                    <hr style="width: 65px; height: 2px; background-color: white; border: none;">
-                    <strong>94 g</strong>
+                    <div class="first-element"><img src="./assets/images/profileImage/cheese.png" alt=""></div>
+                    <div class="second-element">
+                        <p>Fat</p>
+                        <hr class="hr-third-flex">
+                        <strong>94 g</strong>
+                    </div>
                 </div>
             </div>
             <div class="flex-four-item">
@@ -135,61 +149,56 @@ if (trim($_SESSION['email']) == 'admin@gmail.com') {
             <div class="flex-five-item">
                 <a href="http://">
 
-                    <svg width="15" height="19" viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M7.71805 14.2064C12.1751 14.2064 14.237 13.6035 14.4361 11.1838C14.4361 8.76566 12.9986 8.92116 12.9986 5.95426C12.9986 3.63678 10.9152 1 7.71805 1C4.52087 1 2.43754 3.63678 2.43754 5.95426C2.43754 8.92116 1 8.76566 1 11.1838C1.19992 13.6127 3.26183 14.2064 7.71805 14.2064Z" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                        <path opacity="0.4" d="M9.60634 16.7139C8.52818 17.9761 6.84628 17.9911 5.75781 16.7139" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                    <svg width="22" height="16" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M2.51514 7.68825C1.57539 5.46748 2.67366 2.92917 5.7539 2.17809C7.37416 1.78233 9.16257 2.01567 10.5096 2.78267C11.7839 2.03689 13.638 1.78498 15.2565 2.17809C18.3367 2.92917 19.442 5.46748 18.5031 7.68825C17.0405 11.2083 10.5096 13.9197 10.5096 13.9197C10.5096 13.9197 4.02679 11.2494 2.51514 7.68825Z" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
+                        <path opacity="0.4" d="M14.0137 4.44141C14.9508 4.67078 15.6129 5.30386 15.6926 6.04699" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                     <span>Health Information</span>
                     <strong>
                         <svg width="8" height="11" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="7.00867" height="10.3949" transform="translate(0.302734 0.459961)" fill="#3E405C" />
-                            <path d="M1.37412 10.8549C1.66748 10.8549 1.90344 10.7645 2.10751 10.5837L6.90963 6.43704C7.18386 6.19412 7.30503 5.95684 7.3114 5.65742C7.3114 5.36365 7.18386 5.11508 6.90963 4.8778L2.11389 0.725483C1.90344 0.550352 1.6611 0.459961 1.37412 0.459961C0.78741 0.459961 0.302734 0.878018 0.302734 1.39776C0.302734 1.65764 0.430281 1.89491 0.647109 2.09264L4.8115 5.66307L0.647109 9.2222C0.423903 9.41993 0.302734 9.65721 0.302734 9.91708C0.302734 10.4312 0.78741 10.8549 1.37412 10.8549Z" fill="#ABABAB" />
+                            <path d="M1.85647 10.7777C2.15088 10.7777 2.38768 10.6873 2.59249 10.5066L7.41179 6.35989C7.68699 6.11697 7.8086 5.87969 7.815 5.58027C7.815 5.2865 7.68699 5.03793 7.41179 4.80065L2.59889 0.648335C2.38768 0.473203 2.14448 0.382812 1.85647 0.382812C1.26766 0.382812 0.78125 0.800869 0.78125 1.32062C0.78125 1.58049 0.909253 1.81776 1.12686 2.01549L5.30614 5.58592L1.12686 9.14505C0.902853 9.34278 0.78125 9.58006 0.78125 9.83993C0.78125 10.354 1.26766 10.7777 1.85647 10.7777Z" fill="#ABABAB" />
                         </svg></strong>
                 </a>
                 <a href="http://">
 
-                    <svg width="22" height="16" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="21.0195" height="15.91" fill="#3E405C" />
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M2.51514 7.68825C1.57539 5.46748 2.67366 2.92917 5.7539 2.17809C7.37416 1.78233 9.16257 2.01567 10.5096 2.78267C11.7839 2.03689 13.638 1.78498 15.2565 2.17809C18.3367 2.92917 19.442 5.46748 18.5031 7.68825C17.0405 11.2083 10.5096 13.9197 10.5096 13.9197C10.5096 13.9197 4.02679 11.2494 2.51514 7.68825Z" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                        <path opacity="0.4" d="M14.0137 4.44141C14.9508 4.67078 15.6129 5.30386 15.6926 6.04699" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                <svg width="15" height="19" viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M7.71805 14.2064C12.1751 14.2064 14.237 13.6035 14.4361 11.1838C14.4361 8.76566 12.9986 8.92116 12.9986 5.95426C12.9986 3.63678 10.9152 1 7.71805 1C4.52087 1 2.43754 3.63678 2.43754 5.95426C2.43754 8.92116 1 8.76566 1 11.1838C1.19992 13.6127 3.26183 14.2064 7.71805 14.2064Z" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
+  <path opacity="0.4" d="M9.60634 16.7139C8.52818 17.9761 6.84628 17.9911 5.75781 16.7139" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
                     <span>Feedback</span>
 
                     <strong><svg width="8" height="11" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="7.00867" height="10.3949" transform="translate(0.302734 0.459961)" fill="#3E405C" />
-                            <path d="M1.37412 10.8549C1.66748 10.8549 1.90344 10.7645 2.10751 10.5837L6.90963 6.43704C7.18386 6.19412 7.30503 5.95684 7.3114 5.65742C7.3114 5.36365 7.18386 5.11508 6.90963 4.8778L2.11389 0.725483C1.90344 0.550352 1.6611 0.459961 1.37412 0.459961C0.78741 0.459961 0.302734 0.878018 0.302734 1.39776C0.302734 1.65764 0.430281 1.89491 0.647109 2.09264L4.8115 5.66307L0.647109 9.2222C0.423903 9.41993 0.302734 9.65721 0.302734 9.91708C0.302734 10.4312 0.78741 10.8549 1.37412 10.8549Z" fill="#ABABAB" />
+                            <path d="M1.85647 10.7777C2.15088 10.7777 2.38768 10.6873 2.59249 10.5066L7.41179 6.35989C7.68699 6.11697 7.8086 5.87969 7.815 5.58027C7.815 5.2865 7.68699 5.03793 7.41179 4.80065L2.59889 0.648335C2.38768 0.473203 2.14448 0.382812 1.85647 0.382812C1.26766 0.382812 0.78125 0.800869 0.78125 1.32062C0.78125 1.58049 0.909253 1.81776 1.12686 2.01549L5.30614 5.58592L1.12686 9.14505C0.902853 9.34278 0.78125 9.58006 0.78125 9.83993C0.78125 10.354 1.26766 10.7777 1.85647 10.7777Z" fill="#ABABAB" />
                         </svg></strong>
                 </a>
                 <a href="http://">
 
-                    <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="20.6364" height="17.8" fill="#3E405C" />
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3042 11.3818C6.97863 11.3818 4.13867 11.8155 4.13867 13.5524C4.13867 15.2894 6.96061 15.7386 10.3042 15.7386C13.6298 15.7386 16.4689 15.3042 16.4689 13.568C16.4689 11.8318 13.6478 11.3818 10.3042 11.3818Z" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                        <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M10.3036 8.90487C12.486 8.90487 14.2548 7.37845 14.2548 5.49603C14.2548 3.61361 12.486 2.08789 10.3036 2.08789C8.12124 2.08789 6.35159 3.61361 6.35159 5.49603C6.34422 7.37209 8.10158 8.89851 10.2758 8.90487H10.3036Z" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3042 11.3818C6.97863 11.3818 4.13867 11.8155 4.13867 13.5524C4.13867 15.2894 6.96061 15.7386 10.3042 15.7386C13.6298 15.7386 16.4689 15.3042 16.4689 13.568C16.4689 11.8318 13.6478 11.3818 10.3042 11.3818Z" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
+  <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M10.3036 8.90487C12.486 8.90487 14.2548 7.37845 14.2548 5.49603C14.2548 3.61361 12.486 2.08789 10.3036 2.08789C8.12124 2.08789 6.35159 3.61361 6.35159 5.49603C6.34422 7.37209 8.10158 8.89851 10.2758 8.90487H10.3036Z" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
                     <span>Support Service</span>
 
                     <strong><svg width="8" height="11" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="7.00867" height="10.3949" transform="translate(0.302734 0.459961)" fill="#3E405C" />
-                            <path d="M1.37412 10.8549C1.66748 10.8549 1.90344 10.7645 2.10751 10.5837L6.90963 6.43704C7.18386 6.19412 7.30503 5.95684 7.3114 5.65742C7.3114 5.36365 7.18386 5.11508 6.90963 4.8778L2.11389 0.725483C1.90344 0.550352 1.6611 0.459961 1.37412 0.459961C0.78741 0.459961 0.302734 0.878018 0.302734 1.39776C0.302734 1.65764 0.430281 1.89491 0.647109 2.09264L4.8115 5.66307L0.647109 9.2222C0.423903 9.41993 0.302734 9.65721 0.302734 9.91708C0.302734 10.4312 0.78741 10.8549 1.37412 10.8549Z" fill="#ABABAB" />
+                            <path d="M1.85647 10.7777C2.15088 10.7777 2.38768 10.6873 2.59249 10.5066L7.41179 6.35989C7.68699 6.11697 7.8086 5.87969 7.815 5.58027C7.815 5.2865 7.68699 5.03793 7.41179 4.80065L2.59889 0.648335C2.38768 0.473203 2.14448 0.382812 1.85647 0.382812C1.26766 0.382812 0.78125 0.800869 0.78125 1.32062C0.78125 1.58049 0.909253 1.81776 1.12686 2.01549L5.30614 5.58592L1.12686 9.14505C0.902853 9.34278 0.78125 9.58006 0.78125 9.83993C0.78125 10.354 1.26766 10.7777 1.85647 10.7777Z" fill="#ABABAB" />
                         </svg></strong>
                 </a>
                 <a href="http://">
 
-                    <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="21" height="18" fill="#3E405C" />
-                        <path opacity="0.4" d="M13.1393 5.5426V4.84285C13.1393 3.3166 11.6955 2.0791 9.91491 2.0791H5.64928C3.86953 2.0791 2.42578 3.3166 2.42578 4.84285V13.1904C2.42578 14.7166 3.86953 15.9541 5.64928 15.9541H9.92366C11.699 15.9541 13.1393 14.7204 13.1393 13.1986V12.4914" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M19.0828 9.0166H8.54688" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M16.5215 6.83008L19.0835 9.01633L16.5215 11.2033" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                    <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path opacity="0.4" d="M12.1393 4.5426V3.84285C12.1393 2.3166 10.6955 1.0791 8.91491 1.0791H4.64928C2.86953 1.0791 1.42578 2.3166 1.42578 3.84285V12.1904C1.42578 13.7166 2.86953 14.9541 4.64928 14.9541H8.92366C10.699 14.9541 12.1393 13.7204 12.1393 12.1986V11.4914" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M18.0828 8.0166H7.54688" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M15.5215 5.83008L18.0835 8.01633L15.5215 10.2033" stroke="#0601B4" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                     <span>Log out</span>
 
                     <strong><svg width="8" height="11" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="7.00867" height="10.3949" transform="translate(0.302734 0.459961)" fill="#3E405C" />
-                            <path d="M1.37412 10.8549C1.66748 10.8549 1.90344 10.7645 2.10751 10.5837L6.90963 6.43704C7.18386 6.19412 7.30503 5.95684 7.3114 5.65742C7.3114 5.36365 7.18386 5.11508 6.90963 4.8778L2.11389 0.725483C1.90344 0.550352 1.6611 0.459961 1.37412 0.459961C0.78741 0.459961 0.302734 0.878018 0.302734 1.39776C0.302734 1.65764 0.430281 1.89491 0.647109 2.09264L4.8115 5.66307L0.647109 9.2222C0.423903 9.41993 0.302734 9.65721 0.302734 9.91708C0.302734 10.4312 0.78741 10.8549 1.37412 10.8549Z" fill="#ABABAB" />
+                            <path d="M1.85647 10.7777C2.15088 10.7777 2.38768 10.6873 2.59249 10.5066L7.41179 6.35989C7.68699 6.11697 7.8086 5.87969 7.815 5.58027C7.815 5.2865 7.68699 5.03793 7.41179 4.80065L2.59889 0.648335C2.38768 0.473203 2.14448 0.382812 1.85647 0.382812C1.26766 0.382812 0.78125 0.800869 0.78125 1.32062C0.78125 1.58049 0.909253 1.81776 1.12686 2.01549L5.30614 5.58592L1.12686 9.14505C0.902853 9.34278 0.78125 9.58006 0.78125 9.83993C0.78125 10.354 1.26766 10.7777 1.85647 10.7777Z" fill="#ABABAB" />
                         </svg></strong>
                 </a>
             </div>
+    <?php include './app/include/footer.php' ?>
+
         </div>
     <?php else : ?>
         <?php echo $_SESSION['name'] . " ."; ?>
@@ -198,7 +207,6 @@ if (trim($_SESSION['email']) == 'admin@gmail.com') {
 
 
 
-    <?php include './app/include/footer.php' ?>
 </body>
 
 </html>
