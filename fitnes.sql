02ba72b130235bcd8d5a4cc94ea69cc39db1397b
Aibek002 <200103346@stu.sdu.edu.kz>
2024-06-06 22:18:50 +0500

    deleted:    .gcloudignore 	deleted:    app.yaml 	deleted:    fitnes.sql

--- fitnes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE data_registration (
-    id_user INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    date DATETIME DEFAULT CURRENT_TIMESTAMP
-);
